cb86174e5bedb39442eaf97e3a28885b244b8323
Charles Cote <charlescote9@gmail.com>
2013-10-11 12:13:31 -0600

    Fourth revision
    Extracted several methods such as concatenating/splitting the digest,
    producing the key, process the filename.

--- Secure.java
+++ Secure.java
@@ -10,15 +10,34 @@ public class Secure
 
 	private static SecretKeySpec sec_key_spec;
 	private static Cipher sec_cipher;
+	private static Hash digestCreator;
+	
+	private static byte[] file_buffer;
+	private static FileInputStream inFile;
+	private static FileOutputStream outFile;
+	
 	private static final int SHA1_SIZE = 20;
 
 	
 	
-	//decryption function
+	public static void keyGenerator(byte[] seed) {
+		byte[] key;
+		byte[] key128;
+	
+		//generates key
+		digestCreator = new Hash(seed);
+		key = digestCreator.getDigest();
+		key128 = Arrays.copyOfRange(key, 0, 16);
+		sec_key_spec = new SecretKeySpec(key128, "AES");
+	}
+	
+	
+	//encryption/decryption function
 	public static byte[] aes_crypt(byte[] inFileBuffer, int mode) throws Exception {
 		byte[] converted = null;
 		byte[] iv = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
 		IvParameterSpec ivspec = new IvParameterSpec(iv);
+		sec_cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
 		try {
 			//set cipher to decrypt mode
 			if (mode == 1) {
@@ -40,79 +59,96 @@ public class Secure
 		
 		return converted;
 	}
-
 	
+	
+	
+	public static int file_intake (String file_name) throws Exception {
+		FileInputStream inFile;
+		
+		String[] inFilename = file_name.split("\\.(?=[^\\.]+$)");
+		String outFilename = inFilename[0] + "OUT." + inFilename[1];
+		inFile = new FileInputStream(inFilename[0] + "." + inFilename[1]);
+		
+		//reads message file in buffer
+		int file_size = inFile.available();
+		file_buffer = new byte[file_size];
+		inFile.read(file_buffer);
+		inFile.close();
+		
+		outFile = new FileOutputStream(outFilename);
+		
+		return file_size;
+	}
+	
+	
+	
+	public static byte[] combineDigest (byte[] plaintext) {
+		byte[] sha1_hash;
+		byte[] inBuffer;
+		int file_size;
+		
+		digestCreator.update(file_buffer);
+		sha1_hash = digestCreator.getDigest();
+		file_size = file_buffer.length;
+		
+		inBuffer = new byte[file_size + SHA1_SIZE];
+		for (int i = 0; i < file_size; i++) {
+			inBuffer[i] = file_buffer[i];
+		}
+		for (int j = 0; j < SHA1_SIZE; j++) {
+			inBuffer[file_size + j] = sha1_hash[j];
+		}
+
+		return inBuffer;
+	}
+	
+	
+	public static byte[] splitDigest (byte[] decrypted) {
+		byte[] message;
+		byte[] digest;
+		byte[] local_digest;
+		int file_size;
+		
+		file_size = decrypted.length;
+		message = Arrays.copyOfRange(decrypted, 0, (file_size - SHA1_SIZE));
+		digest = Arrays.copyOfRange(decrypted, (file_size - SHA1_SIZE), file_size);
+		digestCreator.update(message);
+		local_digest = digestCreator.getDigest();
+		System.out.println(digestCreator.compareDigests(digest, local_digest));
+		
+		return message;
+	}
 	
 	public static void main (String args[]) throws Exception 
 	{	
-		byte[] key;
-		byte[] key128;
-		FileInputStream inFile;
-		FileOutputStream outFile;
+		byte[] seed;
 		byte[] inBuffer;
 		byte[] outBuffer;
-		byte[] digest;
-		byte[] local_digest;
-		byte[] sha1_hash;	
-		byte[] buffer;
-		byte[] message;
 		byte[] decrypted;
-		Hash digestCreator;
-		String outFilename;
-		int file_size;
+		String file_name;
 		int mode;
 		
 		try {
+			file_name = args[0];
+			seed = args[1].getBytes("us-ascii");
 			mode = Integer.parseInt(args[2]);
+			
 			if ((mode != 1) && (mode != 2)) {
 				return;
 			}
 			
-			//gets filenames and fileinput
-			String[] inFilename = args[0].split("\\.(?=[^\\.]+$)");
-			outFilename = inFilename[0] + "OUT." + inFilename[1];
-			inFile = new FileInputStream(inFilename[0] + "." + inFilename[1]);
+			file_intake(file_name);
 			
-			//reads message file in buffer
-			file_size = inFile.available();
-			buffer = new byte[file_size];
-			inFile.read(buffer);
-			inFile.close();
+			keyGenerator(seed);
 			
-			//generates key
-			digestCreator = new Hash(args[1].getBytes("us-ascii"));
-			key = digestCreator.getDigest();
-			key128 = Arrays.copyOfRange(key, 0, 16);
-			sec_key_spec = new SecretKeySpec(key128, "AES");
-			sec_cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
-			
-			if (args[2].equals("1")) {
-				//creates sha1 digest from message
-				digestCreator.update(buffer);
-				sha1_hash = digestCreator.getDigest();
-				
-				inBuffer = new byte[file_size + SHA1_SIZE];
-				for (int i = 0; i < file_size; i++) {
-					inBuffer[i] = buffer[i];
-				}
-				for (int j = 0; j < SHA1_SIZE; j++) {
-					inBuffer[file_size + j] = sha1_hash[j];
-				}
-				
+			if (mode == 1) {
+				inBuffer = combineDigest(file_buffer);
 				outBuffer = aes_crypt(inBuffer, mode);
-				
 			} else {
-				decrypted = aes_crypt(buffer, mode);
-				file_size = decrypted.length;
-				message = Arrays.copyOfRange(decrypted, 0, (file_size - SHA1_SIZE));
-				digest = Arrays.copyOfRange(decrypted, (file_size - SHA1_SIZE), file_size);
-				digestCreator.update(message);
-				local_digest = digestCreator.getDigest();
-				System.out.println(digestCreator.compareDigests(digest, local_digest));
-				outBuffer = message;
+				decrypted = aes_crypt(file_buffer, mode);
+				outBuffer = splitDigest(decrypted);
 			}
 			
-			outFile = new FileOutputStream(outFilename);
 			outFile.write(outBuffer);
 			outFile.close();
 		
